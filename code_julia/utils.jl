021a79e54ddc2517d5e5423bfbd25ae2ef059048
Rayla Kurosaki 『黒崎 レイラ』 <60267154+rkp1503@users.noreply.github.com>
2023-06-18 23:39:47 -0400

    Update utils.jl

--- code_julia/utils.jl
+++ code_julia/utils.jl
@@ -23,7 +23,7 @@ import .boundary_yz
 include("equilibria/interior.jl")
 import .interior
 
-# const ITER_PARAMS = 1_000_000
+const ITER_PARAMS = 1_000_000
 const ITER = 1_000
 const MAX_VAL = 1e6
 const ROUND = 3
@@ -114,62 +114,56 @@ function my_phase_portrait(sol, title, xaxis, yaxis, variables; zaxis="")
     return figure
 end;
 
-function success_rate(model, D, vars_dict, params_dict, t, tₘₐₓ, eq_type; method="eig")
-    sol_large_c = 0
-    err_c = 0
-    for i in 1:ITER
-        params_keys = collect(keys(params_dict))
-        params_vals = generate_sutible_parameters(params_dict, eq_type, method)
-        params_dict_temp = OrderedDict(zip(params_keys, params_vals))
-        sol = solve_model(model, D, vars_dict, params_dict_temp, t, tₘₐₓ)
-        if !compare_results(sol, params_dict_temp, eq_type)
-            # print_error(sol, params_dict_temp, eq_type)
-            xₛₒₗ, yₛₒₗ, zₛₒₗ = last(sol.u)
-            if (xₛₒₗ > MAX_VAL) || (yₛₒₗ > MAX_VAL) || (zₛₒₗ > MAX_VAL)
-                sol_large_c += 1
-            else
-                err_c += 1
-            end
-        end
-    end
-    # Probability of true solutions. (Considers very large solutions as not true solutions)
-    proba_a = 1-((err_c+sol_large_c)/ITER)
-    # Probability of valid solutions. (Considers very large solutions as true solutions)
-    proba_b = 1-(err_c/ITER)
-    println("Success rate (True solutions): $(100*proba_a)%")
-    println("Success rate (Valid solutions): $(100*proba_b)%")
-    println("Success rate error: $(100*abs(proba_a-proba_b))%")
-    println("")
-end;
 
-function generate_sutible_parameters(params_dict, eq_type, method="eig")
-    while true
-        param_vals = [round(rand(), digits=ROUND) for x in 1:length(params_dict)]
-        if (0 ∉ param_vals) && (1 ∉ param_vals)
-            if eq_exist(param_vals, eq_type)
-                if eq_stable(param_vals, eq_type, method)
-                    return param_vals
-                end
-            end
-        end
-    end
-end;
-
-function generate_valid_parameters(model, D, vars_dict, params_dict, t, tₘₐₓ, eq_type; method="eig")
-    iteration_counter = 1
+function generate_parameters(model, D, vars_dict, params_dict, t, tₘₐₓ, eq_type; debug=false)
+    sol_too_large = 0
+    numerical_differ_analytical = 0
+    iteration = 1
     while true
         params_keys = collect(keys(params_dict))
-        params_vals = generate_sutible_parameters(params_dict, eq_type, method)
+        params_vals = generate_parameters_helper(params_dict, eq_type)
         params_dict_temp = OrderedDict(zip(params_keys, params_vals))
         sol = solve_model(model, D, vars_dict, params_dict_temp, t, tₘₐₓ)
         xₛₒₗ, yₛₒₗ, zₛₒₗ = last(sol.u)
         if !((xₛₒₗ > MAX_VAL) || (yₛₒₗ > MAX_VAL) || (zₛₒₗ > MAX_VAL))
             if compare_results(sol, params_dict_temp, eq_type)
-                # println("Valid parameters found at iteration $(iteration_counter)")
-                return params_vals
+                if !debug
+                    println("Iteration: $(iteration)")
+                    println("Parameters: $(collect(keys(params_dict)))")
+                    println("Parameter values: $(params_vals)")
+                    print_error(sol, params_dict_temp, eq_type)
+                    return params_vals
+                end
+            else
+                numerical_differ_analytical += 1
+            end
+        else
+            sol_too_large += 1
+        end
+        iteration += 1
+        if iteration == ITER_PARAMS
+            break 
+        end
+    end
+    if debug
+        return iteration, sol_too_large, numerical_differ_analytical
+    else
+        println("Failed to find sutible parameters after $(iteration) iterations.")
+        return nothing
+    end
+end;
+
+
+function generate_parameters_helper(params_dict, eq_type)
+    while true
+        param_vals = [round(2*rand(), digits=ROUND) for _ in 1:length(params_dict)]
+        if (0 ∉ param_vals) && (param_vals[3] < 1)
+            if eq_exist(param_vals, eq_type)
+                if eq_stable(param_vals, eq_type)
+                    return param_vals
+                end
             end
         end
-        iteration_counter += 1
     end
 end;
 
@@ -187,26 +181,26 @@ function eq_exist(parameter_values, equilibrium_type)
     end
 end;
 
-function eq_stable(parameter_values, equilibrium_type, method)
+function eq_stable(parameter_values, equilibrium_type)
     if equilibrium_type == "z axial"
-        return axial_z.equilibrium_stable(parameter_values, method)
+        return axial_z.equilibrium_stable(parameter_values)
     elseif equilibrium_type == "xy boundary"
-        return boundary_xy.equilibrium_stable(parameter_values, method)
+        return boundary_xy.equilibrium_stable(parameter_values)
     elseif equilibrium_type == "xz boundary"
-        return boundary_xz.equilibrium_stable(parameter_values, method)
+        return boundary_xz.equilibrium_stable(parameter_values)
     elseif equilibrium_type == "yz boundary"
-        return boundary_yz.equilibrium_stable(parameter_values, "RHC")
+        return boundary_yz.equilibrium_stable(parameter_values)
     else
-        return interior.equilibrium_stable(parameter_values, "RHC")
+        return interior.equilibrium_stable(parameter_values)
     end
 end;
 
 function compare_results(solution, parameters_dict, equilibrium_type)
     xₛₒₗ, yₛₒₗ, zₛₒₗ = last(solution.u)
     E_x, E_y, E_z = get_analytical_solution(parameters_dict, equilibrium_type)
-    x_err = abs(round(E_x, digits=ROUND) - round(xₛₒₗ, digits=ROUND))
-    y_err = abs(round(E_y, digits=ROUND) - round(yₛₒₗ, digits=ROUND))
-    z_err = abs(round(E_z, digits=ROUND) - round(zₛₒₗ, digits=ROUND))
+    x_err = round(abs(E_x-xₛₒₗ), digits=ROUND)
+    y_err = round(abs(E_y-yₛₒₗ), digits=ROUND)
+    z_err = round(abs(E_z-zₛₒₗ), digits=ROUND)
     return (x_err <= TOL_2) && (y_err <= TOL_2) && (z_err <= TOL_2)
 end;
 
@@ -230,30 +224,6 @@ function print_error(solution, parameters_dict, equilibrium_type)
     println("Numerical: ($(round(xₛₒₗ, digits=ROUND)), $(round(yₛₒₗ, digits=ROUND)), $(round(zₛₒₗ, digits=ROUND)))")
     E_x, E_y, E_z = get_analytical_solution(parameters_dict, equilibrium_type)
     println("Analytical: ($(round(E_x, digits=ROUND)), $(round(E_y, digits=ROUND)), $(round(E_z, digits=ROUND)))")
-    println("")
-end;
-
-function analyze(model, D, vars_dict, params_dict, t, tₘₐₓ, eq_type; method="eig")
-    trials = []
-    for i in 1:ITER
-        j = 1
-        while true
-            params_keys = collect(keys(params_dict))
-            params_vals = generate_sutible_parameters(params_dict, eq_type, method)
-            params_dict_temp = OrderedDict(zip(params_keys, params_vals))
-            sol = solve_model(model, D, vars_dict, params_dict_temp, t, tₘₐₓ)
-            if compare_results(sol, params_dict_temp, eq_type)
-                xₛₒₗ, yₛₒₗ, zₛₒₗ = last(sol.u)
-                if !((xₛₒₗ > MAX_VAL) || (yₛₒₗ > MAX_VAL) || (zₛₒₗ > MAX_VAL))
-                    push!(trials, j)
-                    j = 1
-                    break
-                end
-            end
-            j += 1
-        end
-    end
-    println("Expected number of iterations before true solution is found: $(sum(trials)/length(trials))")
     println("")
 end;
 
