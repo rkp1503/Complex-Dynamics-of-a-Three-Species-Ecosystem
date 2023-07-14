fbc3faa2c327554de4ca7f1731b025a58d5e8763
Rayla Kurosaki 『黒崎 レイラ』 <60267154+rkp1503@users.noreply.github.com>
2023-07-14 18:44:43 -0400

    Update boundary_yz.jl

--- code_julia/equilibria/boundary_yz.jl
+++ code_julia/equilibria/boundary_yz.jl
@@ -7,63 +7,63 @@ module boundary_yz
 
 import Polynomials: Polynomial, roots
 
-function equilibrium_exist(param_vals)
-    r₂₁, r₃₁, p, ϕ₁₂, ϕ₂₁, ϕ₁₃, u₁, u₂, u₃, u₄ = param_vals
-    x, y, z = analytical_solution(param_vals)
-    cond1 = y > (u₂*(u₄-r₃₁))/((u₃-u₄+r₃₁)*(1-p))
-    cond2 = (r₂₁*r₃₁*u₂)/(u₁*(r₃₁-u₄)*(1-p)) > 1
-    return all([cond1, cond2])
+function get_equilibria(parameter_values)
+    equilibria = get_analytical_solutions(parameter_values)
+    r₂₁, r₃₁, p, ϕ₁₂, ϕ₂₁, ϕ₁₃, u₁, u₂, u₃, u₄ = parameter_values
+    for (i, equilibrium) in enumerate(equilibria)
+        x, y, z = equilibrium
+        # if !(z > 0)
+        #     removeat!(equilibria, i)
+        # end
+        cond1 = y > (u₂*(u₄-r₃₁))/((u₃-u₄+r₃₁)*(1-p))
+        cond2 = (r₂₁*r₃₁*u₂)/(u₁*(r₃₁-u₄)*(1-p)) > 1
+        if !all([cond1, cond2])
+            deleteat!(equilibria, i)
+        end
+    end
+    return equilibria
 end;
 
-function equilibrium_stable(param_vals)
-    r₂₁, r₃₁, p, ϕ₁₂, ϕ₂₁, ϕ₁₃, u₁, u₂, u₃, u₄ = param_vals
-    x, y, z = analytical_solution(param_vals)
-    j₁₁ = 1+ϕ₁₂*y^2-ϕ₁₃*z
-    j₂₂ = r₂₁*(1-2*y)-((u₁*u₂*(1-p)*z)/(u₂+(1-p)*y)^2)
-    j₂₃ = -((u₁*(1-p)*y)/(u₂+(1-p)*y))
-    j₃₂ = ((u₂*u₃*(1-p)*z)/(u₂+(1-p)*y)^2)
-    j₃₃ = r₃₁*(1-2*z)-u₄+((u₃*(1-p)*y)/(u₂+(1-p)*y))
-    C₂ = -j₁₁-j₂₂-j₃₃
-    C₁ = j₁₁*j₂₂+j₁₁*j₃₃+j₂₂*j₃₃-j₂₃*j₃₂
-    C₀ = j₁₁*(j₂₃*j₃₂-j₂₂*j₃₃)
-    cond1 = C₂ > 0
-    cond2 = C₁ > 0
-    cond3 = C₀ > 0
-    cond4 = C₂*C₁ > C₀
-    return all([cond1, cond2, cond3, cond4])
+function get_stable_equilibria!(equilibria, parameter_values)
+    r₂₁, r₃₁, p, ϕ₁₂, ϕ₂₁, ϕ₁₃, u₁, u₂, u₃, u₄ = parameter_values
+    for (i, equilibrium) in enumerate(equilibria)
+        x, y, z = equilibrium
+        j₁₁ = 1+ϕ₁₂*y^2-ϕ₁₃*z
+        j₂₂ = r₂₁*(1-2*y)-((u₁*u₂*(1-p)*z)/(u₂+(1-p)*y)^2)
+        j₂₃ = -((u₁*(1-p)*y)/(u₂+(1-p)*y))
+        j₃₂ = ((u₂*u₃*(1-p)*z)/(u₂+(1-p)*y)^2)
+        j₃₃ = r₃₁*(1-2*z)-u₄+((u₃*(1-p)*y)/(u₂+(1-p)*y))
+        C₂ = -j₁₁-j₂₂-j₃₃
+        C₁ = j₁₁*j₂₂+j₁₁*j₃₃+j₂₂*j₃₃-j₂₃*j₃₂
+        C₀ = j₁₁*(j₂₃*j₃₂-j₂₂*j₃₃)
+        cond1 = C₂ > 0
+        cond2 = C₁ > 0
+        cond3 = C₀ > 0
+        cond4 = C₂*C₁ > C₀
+        if !all([cond1, cond2, cond3, cond4])
+            deleteat!(equilibria, i)
+        end
+    end
+    return nothing
 end;
 
-function analytical_solution(param_vals)
-    r₂₁, r₃₁, p, ϕ₁₂, ϕ₂₁, ϕ₁₃, u₁, u₂, u₃, u₄ = param_vals
-    y = -1
-    z = -1
+function get_analytical_solutions(parameter_values)
+    sols_real = []
+    r₂₁, r₃₁, p, ϕ₁₂, ϕ₂₁, ϕ₁₃, u₁, u₂, u₃, u₄ = parameter_values
     Y₀ = u₂*(r₂₁*r₃₁*u₂+u₁*(u₄-r₃₁)*(1-p))
     Y₁ = r₂₁*r₃₁*u₂*(2*(1-p)-u₂)+u₁*(u₄-u₃-r₃₁)*(1-p)^2
     Y₂ = r₂₁*r₃₁*((1-p)-2*u₂)*(1-p)
     Y₃ = -r₂₁*r₃₁*(1-p)^2
-    coeff = [Y₀, Y₁, Y₂, Y₃]
     if Y₀ > 0
-        poly_roots = roots(Polynomial(coeff, :y))
-        for root in poly_roots
+        for root in roots(Polynomial([Y₀, Y₁, Y₂, Y₃], :y))
             if (real(root) > 0) && (imag(root) == 0)
                 y = real(root)
                 z = 1+((((u₃*(1-p)*y)/(u₂+(1-p)*y))-u₄)/r₃₁)
-                break
+                push!(sols_real, (0, y, z))
             end
         end
     end
-    return (0, y, z)
-end;
-
-function descarte(lst)
-    unit_lst = [n/abs(n) for n in lst]
-    sign_changes = 0
-    for i in 1:(length(unit_lst)-1)
-        if unit_lst[i]*unit_lst[i+1] < 0
-            sign_changes += 1
-        end
-    end
-    return isodd(sign_changes)
+    return sols_real
 end;
 
 end
