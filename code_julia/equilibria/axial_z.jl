1f9a662b53f37e2dd586a6c790228bf68becd53b
Rayla Kurosaki 『黒崎 レイラ』 <60267154+rkp1503@users.noreply.github.com>
2023-07-14 18:44:39 -0400

    Update axial_z.jl

--- code_julia/equilibria/axial_z.jl
+++ code_julia/equilibria/axial_z.jl
@@ -5,24 +5,40 @@ GitHub: https://github.com/rkp1503
 
 module axial_z
 
-function equilibrium_exist(param_vals)
-    r₂₁, r₃₁, p, ϕ₁₂, ϕ₂₁, ϕ₁₃, u₁, u₂, u₃, u₄ = param_vals
-    cond = r₃₁ > u₄
-    return all([cond])
+function get_equilibria(parameter_values)
+    equilibria = get_analytical_solutions(parameter_values)
+    r₂₁, r₃₁, p, ϕ₁₂, ϕ₂₁, ϕ₁₃, u₁, u₂, u₃, u₄ = parameter_values
+    for (i, equilibrium) in enumerate(equilibria)
+        x, y, z = equilibrium
+        # if !(z > 0)
+        #     removeat!(equilibria, i)
+        # end
+        cond = r₃₁ > u₄
+        if !all([cond])
+            deleteat!(equilibria, i)
+        end
+    end
+    return equilibria
 end;
 
-function equilibrium_stable(param_vals)
-    r₂₁, r₃₁, p, ϕ₁₂, ϕ₂₁, ϕ₁₃, u₁, u₂, u₃, u₄ = param_vals
-    cond1 = (u₄/r₃₁)+(1/ϕ₁₃) < 1
-    cond2 = (u₄/r₃₁)+((r₂₁*u₂)/(u₁*(1-p))) < 1
-    cond3 = (u₄/r₃₁)<(1/2)
-    return all([cond1, cond2, cond3])
+function get_stable_equilibria!(equilibria, parameter_values)
+    r₂₁, r₃₁, p, ϕ₁₂, ϕ₂₁, ϕ₁₃, u₁, u₂, u₃, u₄ = parameter_values
+    for (i, equilibrium) in enumerate(equilibria)
+        x, y, z = equilibrium
+        cond1 = (u₄/r₃₁)+(1/ϕ₁₃) < 1
+        cond2 = (u₄/r₃₁)+((r₂₁*u₂)/(u₁*(1-p))) < 1
+        cond3 = (u₄/r₃₁)<(1/2)
+        if !all([cond1, cond2, cond3])
+            deleteat!(equilibria, i)
+        end
+    end
+    return nothing
 end;
 
-function analytical_solution(param_vals)
-    r₂₁, r₃₁, p, ϕ₁₂, ϕ₂₁, ϕ₁₃, u₁, u₂, u₃, u₄ = param_vals
+function get_analytical_solutions(parameter_values)
+    r₂₁, r₃₁, p, ϕ₁₂, ϕ₂₁, ϕ₁₃, u₁, u₂, u₃, u₄ = parameter_values
     z = 1-(u₄/r₃₁)
-    return (0, 0, z)
+    return [(0, 0, z)]
 end;
     
 end
