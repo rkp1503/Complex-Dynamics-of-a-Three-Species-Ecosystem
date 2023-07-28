c3fac1ab61d813ab6de37a25b15da2634f273f43
Rayla Kurosaki 『黒崎 レイラ』 <60267154+rkp1503@users.noreply.github.com>
2023-07-28 04:23:15 -0400

    Update GenerateFigures.jl

--- code_julia/utils/GenerateFigures.jl
+++ code_julia/utils/GenerateFigures.jl
@@ -47,10 +47,10 @@ function my_phase_portraits(solutions, xaxis, yaxis, zaxis)
     solution_x = solutions'[:, 1]
     solution_y = solutions'[:, 2]
     solution_z = solutions'[:, 3]
-    fig_a = my_3D_phase_portrait(solutions, "3D Phase Portrait", xaxis, yaxis, zaxis)
-    fig_b = my_2D_phase_portrait(solution_x, solution_y, "Species X and Y", xaxis, yaxis)
-    fig_c = my_2D_phase_portrait(solution_x, solution_z, "Species X and Z", xaxis, zaxis)
-    fig_d = my_2D_phase_portrait(solution_y, solution_z, "Species Y and Z", yaxis, zaxis)
+    fig_a = my_3D_phase_portrait(solutions, "3D Phase Portrait", xaxis, yaxis, zaxis; detailed_fig=true)
+    fig_b = my_2D_phase_portrait(solution_x, solution_y, "Species X and Y", xaxis, yaxis; detailed_fig=true)
+    fig_c = my_2D_phase_portrait(solution_x, solution_z, "Species X and Z", xaxis, zaxis; detailed_fig=true)
+    fig_d = my_2D_phase_portrait(solution_y, solution_z, "Species Y and Z", yaxis, zaxis; detailed_fig=true)
     l = @layout [a b ; c d]
     return plot(fig_a, fig_b, fig_c, fig_d, layout=l)
 end;
@@ -83,10 +83,10 @@ function my_bifurcation_diagrams(solutions, data, variables, bifurcartion_parame
     title = "Time Evolution of Each Species"
 	xaxis = "Time in Days"
 	yaxis = "Population size"
-    fig_a = my_plot(solutions, title, xaxis, yaxis, legend_data)
-    fig_b = my_bifurcation_diagram(x_arr_s, x_arr_u, x, bifurcartion_parameter, "black")
-    fig_c = my_bifurcation_diagram(y_arr_s, y_arr_u, y, bifurcartion_parameter, "red")
-    fig_d = my_bifurcation_diagram(z_arr_s, z_arr_u, z, bifurcartion_parameter, "blue")
+    fig_a = my_plot(solutions, title, xaxis, yaxis, legend_data; detailed_fig=true)
+    fig_b = my_bifurcation_diagram(x_arr_s, x_arr_u, x, bifurcartion_parameter, "black"; detailed_fig=true)
+    fig_c = my_bifurcation_diagram(y_arr_s, y_arr_u, y, bifurcartion_parameter, "red"; detailed_fig=true)
+    fig_d = my_bifurcation_diagram(z_arr_s, z_arr_u, z, bifurcartion_parameter, "blue"; detailed_fig=true)
     l = @layout [a b ; c d]
     return plot(fig_a, fig_b, fig_c, fig_d, layout=l)
 end;
